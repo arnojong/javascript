d0caa523daa12e8af66ab2a2ac0765ba34683d49
Arno de Jong <arno@elephantcs.nl>
2018-10-05 16:22:26 +0200

    Adds percentage width function

--- assets/src/js/app.js
+++ assets/src/js/app.js
@@ -1,13 +1,29 @@
 document.addEventListener("DOMContentLoaded", function() {
     const userForm = document.querySelector('.user-form');
     const userContainerList = document.querySelector('.user-container__list');
-
-    const nameValue = document.querySelector('.user-form__name').value;
-    const emailValue = document.querySelector('.user-form__email').value;
-
+    
     const createLi = () => {
+        const nameValue = document.querySelector('.user-form__name').value;
+        const addressValue = document.querySelector('.user-form__address').value;
+        const emailValue = document.querySelector('.user-form__email').value;
+
         const userItem = document.createElement('li');
         const userList = document.createElement('ul');
+        userList.classList.add('user-container__inner-list');
+
+        const addUserUtility = (innerContent) => {
+            const userName = document.createElement('li');
+            userName.classList.add('user-container__utility');
+            const userNameParagraph = document.createElement('p');
+            userNameParagraph.textContent = innerContent;
+            userName.appendChild(userNameParagraph);
+            userList.appendChild(userName);
+        };
+
+        addUserUtility(nameValue);
+        addUserUtility(addressValue);
+        addUserUtility(emailValue);
+
         userItem.appendChild(userList)
         
         userContainerList.appendChild(userItem);
@@ -16,9 +32,9 @@ document.addEventListener("DOMContentLoaded", function() {
     userForm.addEventListener('submit', (e) => {
         e.preventDefault();
 
-        console.log('test');
+        createLi();
 
-        // createLi();
+        userForm.reset();
     }); 
     
 });
