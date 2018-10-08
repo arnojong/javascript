a5c5f462b2c22023caeaa823504f511c644e1d36
Arno de Jong <arno@elephantcs.nl>
2018-10-08 14:32:59 +0200

    Adds car array and semi finished filter

---
+++ assets/src/js/app.js
@@ -0,0 +1,49 @@
+const typeList = document.querySelector('.car-type__list');
+const form = document.querySelector('.car-search');
+const typeExist = false;
+
+const cars = [
+    {
+        type: 'tesla',
+        doors: 5,
+        color: 'green'
+    },
+    {
+        type: 'ferrari',
+        doors: 3,
+        color: 'red'
+    },
+    {
+        type: 'hummer',
+        doors: 5,
+        color: 'black'
+    },
+    {
+        type: 'skoda',
+        doors: 5,
+        color: 'white'
+    }
+];
+
+console.log(cars);
+
+for (let car of cars) {
+    const li = document.createElement('li');
+    li.textContent = car.type;
+    
+    typeList.appendChild(li);
+}
+
+form.addEventListener('submit', (e) => {
+
+    e.preventDefault();
+
+    const searchInput = document.querySelector('.car-search__type').value;
+
+    if (searchInput === car.type) {
+        alert('Is aanwezig');
+    } else {
+        alert('Hebben we niet pik');
+    }
+
+});
